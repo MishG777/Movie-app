77b931d8d11215e0fd322f30728e2f7e1bc5647e
misho <mishiko8gb@gmail.com>
2022-01-24 21:12:25 +0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Movie from "./components/Movie";
+import "./stypes.css";
 
-function App() {
+const FEATURED_API =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1";
+
+const SEARCH_API =
+  "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+
+export default function App() {
+  const hello = (e) => {
+    console.log(e);
+  };
+  hello();
+
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    axios.get(FEATURED_API).then((res) => {
+      setMovies(res.data.results);
+    });
+  }, []);
+
+  const getMovies = () => {
+    axios.get(SEARCH_API + searchTerm).then((res) => {
+      setMovies(res.data.results);
+    });
+  };
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+
+    if (searchTerm) {
+      getMovies();
+      setSearchTerm("");
+    }
+  };
+
+  const handleOnChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <form onSubmit={handleOnSubmit}>
+          <input
+            type="search"
+            placeholder="Search..."
+            className="search"
+            value={searchTerm}
+            onChange={handleOnChange}
+          />
+        </form>
       </header>
-    </div>
+      <div className="movie-container">
+        {movies.map((each) => (
+          <Movie {...each} key={each.id} />
+        ))}
+      </div>
+    </>
   );
 }
 
-export default App;
+// https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1
+//"https://image.tmdb.org/t/p/w1280"
+//
---
+++ src/components/Movie.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const IMG_API = "https://image.tmdb.org/t/p/w1280";
+
+const Movie = ({ poster_path, title, overview, vote_average }) => {
+  return (
+    <div className="movie">
+      <div className="movie-header">
+        <img src={IMG_API + poster_path} alt={title} />
+        <div className="movie-info">
+          <h3>{title}</h3>
+          <span>{vote_average}</span>
+
+          <div className="movie-over">
+            <h2>Overview</h2>
+            <p>{overview}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Movie;
